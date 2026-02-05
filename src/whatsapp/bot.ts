8ed6a0a420ac3204d780a969cc9d82d551ba4aff
sidneyjfg <sidneyjunio191@gmail.com>
2026-02-05 11:44:53 -0300

    Mudança de flags no browser

--- src/whatsapp/bot.ts
+++ src/whatsapp/bot.ts
@@ -8,13 +8,10 @@ export const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
+    executablePath: '/usr/bin/chromium-browser', // ou chromium
     args: [
       '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-gpu',
-      '--no-zygote',
-      '--single-process'
+      '--disable-setuid-sandbox'
     ]
   }
 });
